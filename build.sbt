f47078cce9beb97f12b515ab211c00a28c053137
letusfly85 <letusfly95@gmail.com>
2013-12-15 18:57:44 +0900

    add dependencies and manager class

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "xlsaccessor"
 version := "1.0"
 
 libraryDependencies += "org.apache.poi" % "poi" % "3.7"
+
+libraryDependencies += "org.apache.poi" % "poi-ooxml" % "3.7"
--- src/main/scala/com/jellyfish85/xlsaccessor/manager/XlsManager.scala
+++ src/main/scala/com/jellyfish85/xlsaccessor/manager/XlsManager.scala
@@ -1,5 +1,8 @@
 package com.jellyfish85.xlsaccessor.manager
 
+import org.apache.poi.ss.usermodel.{WorkbookFactory, Workbook}
+import java.io.{IOException, FileNotFoundException, File, FileInputStream}
+
 /**
  * == XlsManager ==
  *
@@ -9,5 +12,20 @@ package com.jellyfish85.xlsaccessor.manager
  *
  */
 class XlsManager {
+
+  /**
+   * == workbook ==
+   *
+   *
+   * @author wada shunsuke
+   * @since  2013/12/15
+   * @param filePath
+   * @return
+   */
+  @throws(classOf[FileNotFoundException])
+  @throws(classOf[IOException])
+  def workbook(filePath: String): Workbook = {
+    WorkbookFactory.create(new FileInputStream(new File(filePath)))
+  }
 
 }
