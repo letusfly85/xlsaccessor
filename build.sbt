e2f9a15fcb8b33f35d1ba12c26fe3dec18643d6e
letusfly85 <letusfly95@gmail.com>
2013-12-15 18:35:08 +0900

    add dependencies

--- build.sbt
+++ build.sbt
@@ -1,4 +1,5 @@
 name := "xlsaccessor"
 
 version := "1.0"
-    
+
+libraryDependencies += "org.apache.poi" % "poi" % "3.7"
---
+++ src/main/scala/com/jellyfish85/xlsaccessor/manager/XlsManager.scala
@@ -0,0 +1,13 @@
+package com.jellyfish85.xlsaccessor.manager
+
+/**
+ * == XlsManager ==
+ *
+ *
+ * @author wada shunsuke
+ * @since  2013/12/15
+ *
+ */
+class XlsManager {
+
+}
