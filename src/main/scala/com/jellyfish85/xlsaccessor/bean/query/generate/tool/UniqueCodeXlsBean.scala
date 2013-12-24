6c06069332dd1f30c7375b98dd13a7a6f189ef8c
letusfly85 <letusfly95@gmail.com>
2013-12-24 22:05:23 +0900

    add unique code data accessor

---
+++ src/main/scala/com/jellyfish85/xlsaccessor/bean/query/generate/tool/UniqueCodeXlsBean.scala
@@ -0,0 +1,13 @@
+package com.jellyfish85.xlsaccessor.bean.query.generate.tool
+
+/**
+ * == UniqueCodeXlsBean ==
+ *
+ *
+ * @author wada shunsuke
+ * @since  2013/12/25
+ *
+ */
+class UniqueCodeXlsBean extends GeneralXlsBean {
+
+}
---
+++ src/main/scala/com/jellyfish85/xlsaccessor/dao/query/generate/tool/UniqueCodeXlsDao.scala
@@ -0,0 +1,18 @@
+package com.jellyfish85.xlsaccessor.dao.query.generate.tool
+
+/**
+ * == UniqueCodeXlsDao ==
+ *
+ * find unique code xls data and return its bean list
+ *
+ * @author wada shunsuke
+ * @since  2013/12/24
+ *
+ */
+class UniqueCodeXlsDao {
+
+  def findAll() {
+
+  }
+
+}
